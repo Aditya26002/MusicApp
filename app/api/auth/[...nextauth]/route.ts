d914276e9ccdc05f1e28605c3c9d092ea2e7ca52
Aditya <aditya2012058@akgec.ac.in>
2024-11-05 10:30:46 +0530

    Implement authentication and upvote/downvote functionality with Prisma integration

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,4 @@
+import { prismaClient } from "@/app/lib/db";
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
@@ -8,6 +9,23 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_SECRET as string,
     }),
   ],
+  secret: process.env.NEXTAUTH_SECRET ?? "secret",
+  callbacks: {
+    async signIn(params) {
+      if (!params.user.email) {
+        return false;
+      }
+      try {
+        await prismaClient.user.create({
+          data: {
+            email: params.user.email,
+            provider: "GOOGLE",
+          },
+        });
+      } catch (error) {}
+      return true;
+    },
+  },
 });
 
 export { handler as GET, handler as POST };
